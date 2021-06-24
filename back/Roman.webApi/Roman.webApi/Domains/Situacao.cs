2e1421146df27e0787652373acb9cf8bf3706809
Taila <taila.franco@aluno.ufabc.edu.br>
2021-06-24 14:02:58 -0300

    Retirado situacao1

---
+++ back/Roman.webApi/Roman.webApi/Domains/Situacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Roman.webApi.Domains
+{
+    public partial class Situacao
+    {
+        public Situacao()
+        {
+            Temas = new HashSet<Tema>();
+        }
+
+        public int IdSituacao { get; set; }
+        [Required(ErrorMessage = "O campo estado é obrigatório")]
+        public string Estado { get; set; }
+
+        public virtual ICollection<Tema> Temas { get; set; }
+    }
+}
---
+++ back/Roman.webApi/Roman.webApi/Interfaces/IProjetoRepository.cs
@@ -0,0 +1,30 @@
+﻿using Roman.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roman.webApi.Interfaces
+{
+    interface IProjetoRepository
+    {
+        // CRUD 
+        // Cadastra uma novo Projeto
+        void Cadastrar(Projeto novoProjeto);
+
+        //Lista todos os Projetos com seus respectivos temas
+        List<Projeto> ListarTodos();
+
+        //Lista todos os Projetos com seus respectivos temas e pelo id do professor que cadastrou o projeto
+        List<Projeto> ListarTodosPorProfessor(int id);
+
+        //Busca um Projeto pelo id
+        Projeto BuscarPorId(int id);
+
+        //Atualiza um Projeto pelo id
+        void Atualizar(int id, Projeto atualizaProjeto);
+
+        // Deleta um Projeto pelo id
+        void Deletar(int id);
+    }
+}
